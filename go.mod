fef7a7825d6ac6b58bf9c9b39be715bb9059c9cb
Oskar Kwoczka <oskar.kwoczka@gmail.com>
2023-07-13 20:32:16 +0200

    added dog package

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/OskarKwoczka/puppy
 
 go 1.20
+
+require github.com/OskarKwoczka/dog v0.0.0-20230713182825-2c68996ede78 // indirect
--- puppy.go
+++ puppy.go
@@ -1,8 +1,16 @@
 package puppy
 
+import (
+	"github.com/OskarKwoczka/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 func Barks() string {
 	return "WOOF! WOOF! WOOF!"
+}
+
+func BigBark() string {
+	return dog.WhenGrowUp(Barks())
 }
